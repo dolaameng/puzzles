6e9c49b9b6add6010c589f0a4a6d21eb6bacacc3
dolaameng <dolabot@gmail.com>
2015-02-01 21:43:44 +0800

    finish barn1

--- usaco/barn1/barn1.cpp
+++ usaco/barn1/barn1.cpp
@@ -9,26 +9,80 @@ LANG: C++11
 #include <string>
 #include <cassert>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+class Block {
+public:
+	int start;
+	int end;
+	Block(int s, int e): start(s), end(e) {};
+};
+
+
+
+vector<Block> repair(const vector<Block> & blocks, const int n) {
+	if (blocks.size() <= n) {
+		return blocks;
+	} else {
+		int min_dist_i = 0;
+		int min_dist = 1000000;
+		for (int i = 0; i <= blocks.size() - 2; ++i) {
+			int dist = blocks[i+1].start - blocks[i].end;
+			if (dist < min_dist) {
+				min_dist = dist;
+				min_dist_i = i;
+			}
+		}
+		vector<Block> new_blocks;
+		for (int i = 0; i <= blocks.size() - 1; ++i) {
+			if (i == min_dist_i) {
+				Block b(blocks[i].start, blocks[i+1].end);
+				i += 1;
+				new_blocks.push_back(b);
+			} else {
+				new_blocks.push_back(blocks[i]);
+			}
+		}
+		return repair(new_blocks, n);
+	}
+}
+
 int main () {
-	//parse inputs
+	// parse inputs
 	ifstream fin ("barn1.in");
 	ofstream fout("barn1.out");
 	int M, S, C;
 	fin >> M >> S >> C;
-	vector<int> cow_stalls(S);
+	vector<int> cow_stalls(C);
 	for (int c = 0; c <= C-1; ++c) {
 		int cow;
 		fin >> cow;
-		cow_stalls[cow] = 1;
+		cow_stalls[c] = cow;
 	}
+	sort(cow_stalls.begin(), cow_stalls.end());
+	// find stall continous blocks
+	vector<Block> blocks;
+	int start = 0;
+	int end = 0;
+	for (int i = 1; i <= cow_stalls.size() - 1; ++i) {
+		if (cow_stalls[i] == cow_stalls[i-1]+1) {
+			end = i;
+		} else {
+			blocks.push_back(Block(cow_stalls[start], cow_stalls[end]));
+			start = i;
+			end = i;
+		}
+	}
+	blocks.push_back(Block(cow_stalls[start], cow_stalls[end]));
 
-	for(int s = 0; s <= S-1; ++s) {
-		cout << cow_stalls[s];
+	vector<Block> barn = repair(blocks, M);
+	int n_blocks = 0;
+	for (const Block & block : barn) {
+		n_blocks += block.end - block.start + 1;
 	}
-	cout << endl;
+	fout << n_blocks << endl;
 
 	return 0;
 }
--- usaco/resource.md
+++ usaco/resource.md
@@ -3,3 +3,6 @@
 	- from n-1 partial solution to n partial (only take the best solution of previous step): think about what an OPTIMAL solution at step n should look like
 	- how to prove it is correct: sub-optimal always leads to optimal
 	- [discussion on "greedy stays ahead"](http://web.stanford.edu/class/archive/cs/cs161/cs161.1138/handouts/120%20Guide%20to%20Greedy%20Algorithms.pdf)
+3. [Crafting Winning Solutions - Section 1.3](http://train.usaco.org/usacogate)
+	- Napkin calculation - 100M steps per second on a modern machine.
+	
