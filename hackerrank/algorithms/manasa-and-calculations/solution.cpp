5fa7905e9da9ff30dc2e8bf508deb633aa60ab7e
dolaameng <dolabot@gmail.com>
2015-03-19 20:56:54 +0800

    TODO: manasa and calculations

---
+++ hackerrank/algorithms/manasa-and-calculations/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int N; cin >> N;
+	vector<int> ps;
+	vector<int> as;
+	vector<int> bs;
+	for (int i = 0; i < N; ++i) {
+		cin >> ps[i] >> as[i] >> bs[i];
+	}
+	long long sum = 0;
+	int A, B;
+	for (int i = 0; i < N; ++i) {
+		
+	}
+}
